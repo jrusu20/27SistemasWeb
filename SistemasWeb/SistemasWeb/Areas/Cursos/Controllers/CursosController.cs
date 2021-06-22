80f6bbf2c8cc698b8e81f1da238b85e35d17f394
jrusu20 <jrusu20@gmail.com>
2021-06-22 11:28:04 -0600

    Controlador de Cursos

---
+++ SistemasWeb/SistemasWeb/Areas/Cursos/Controllers/CursosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemasWeb.Areas.Cursos.Controllers
+{
+    public class CursosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
